4905d41493af59f044b527ae621d3cfae809964b
Stepp1 <sfaragg@gmail.com>
2020-11-09 22:35:38 -0300

    bug fixes, new inteface to manipulate data, added an examnple

--- Tests/ReMasFrametests.py
+++ Tests/ReMasFrametests.py
@@ -1,17 +1,25 @@
 import fsspec
+
 from src.ReMasFrame import ReMasFrame
-from io import StringIO
-import pandas as pd
-import geopandas as gpd
 
 mapper = fsspec.get_mapper('gcs://data-projectx/')
 
+nasa_df = ReMasFrame()
 
-def read_bytes_string(byte_file, encoding='utf-8'):
-    s = str(byte_file,'utf-8')
-    return StringIO(s)
-
-
-nasa_df = pd.read_csv("gs://data-projectx/nasa/landslides.csv")
-nasa_df = ReMasFrame(nasa_df, geometry=gpd.points_from_xy(nasa_df.longitude, nasa_df.latitude))
+# changes Point to Polygon
 nasa_df.box(2, inplace=True)
+
+# Returns products that we use as a dict
+products = nasa_df.get_products()
+
+# Choose CHIRPS Daily Precip
+chirps = products['weather']['chirps']
+
+# Choose an idx --> a landslide (or filter the geodataframe :D)
+idx_test = 0
+
+# Returns start and end date of a 4 day interval
+start_date, end_date = nasa_df.date_interval(nasa_df.event_date[idx_test], delta=2)
+
+# Searches scenes between start and end date
+scenes, ctx = search_scenes(nasa_df.geometry[idx_test], chirps[id], start_date=start_date, end_date=end_date, limit=10)
